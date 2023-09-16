0e8eb9f8df3df2ae8c94727cd21d4eb1a1104af5
Joel Storr <joel.storr@ewe.net>
2023-09-16 21:53:02 +0200

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Joel Storr on 16.09.23.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
