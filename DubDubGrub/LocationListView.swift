4603cc9660b7313375569723be569680c8dc73aa
Joel Storr <joel.storr@ewe.net>
2023-09-17 15:14:25 +0200

    Added Locations List

--- DubDubGrub/LocationListView.swift
+++ DubDubGrub/LocationListView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct LocationListView: View {
     var body: some View {
-        Text("Location List View")
+        NavigationView{
+            
+            
+            List{
+                ForEach( 0..<10){ item in
+                    HStack{
+                        Image("default-square-asset")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                            .padding(.vertical, 8)
+                        VStack{
+                            Text("Test location name")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.75)
+                            HStack{
+                                
+                            }
+                        }
+                    }
+                }
+            }
+                .navigationTitle("Grub Spots")
+            
+            
+            
+        }
     }
 }
 
