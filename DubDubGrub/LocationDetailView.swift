e102e220c1bdd4c4db90ae083d8465cd0745c7b8
Joel Storr <joel.storr@ewe.net>
2023-09-18 12:22:32 +0200

    Added Navigation links between views

--- DubDubGrub/LocationDetailView.swift
+++ DubDubGrub/LocationDetailView.swift
@@ -14,83 +14,86 @@ struct LocationDetailView: View {
     
     
     var body: some View {
-        NavigationView{
-            VStack(spacing: 16){
-                Image("default-banner-asset")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 120)
-                
-                HStack{
-                    Label("123 Main Street", systemImage: "mappin.and.ellipse")
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                    Spacer()
-                }
+        
+        VStack(spacing: 16){
+            Image("default-banner-asset")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 120)
+            
+            HStack{
+                Label("123 Main Street", systemImage: "mappin.and.ellipse")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                Spacer()
+            }
+            .padding(.horizontal)
+            Text("This is a test description. This is a test description. This is a test description.This is a test description.This is a test description.")
+                .lineLimit(3)
+                .minimumScaleFactor(0.75)
+                .frame(height: 70)
                 .padding(.horizontal)
-                Text("This is a test description. This is a test description. This is a test description.This is a test description.This is a test description.")
-                    .lineLimit(3)
-                    .minimumScaleFactor(0.75)
-                    .padding(.horizontal)
-                
-                
-                ZStack{
-                    Capsule()
-                        .frame(height: 80)
-                        .foregroundColor(Color(.secondarySystemBackground))
-                    HStack(spacing: 20){
-                        Button{
-                            
-                        }label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "location.fill")
-                        }
+            
+            
+            ZStack{
+                Capsule()
+                    .frame(height: 80)
+                    .foregroundColor(Color(.secondarySystemBackground))
+                HStack(spacing: 20){
+                    Button{
                         
-                        Link(destination: URL(string: "www.apple.com")!) {
-                            
-                            LocationActionButton(color: .brandPrimary, imageName: "network")
-                        }
-                        
-                        Button{
-                            
-                        }label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
-                        }
-                        Button{
-                            
-                        }label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "person.fill.checkmark")
-                        }
+                    }label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "location.fill")
                     }
                     
+                    Link(destination: URL(string: "www.apple.com")!) {
+                        
+                        LocationActionButton(color: .brandPrimary, imageName: "network")
+                    }
+                    
+                    Button{
+                        
+                    }label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
+                    }
+                    Button{
+                        
+                    }label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "person.fill.checkmark")
+                    }
                 }
-                
-                Text("Who's Here?")
-                    .bold()
-                    .font(.title2)
-                
-                LazyVGrid(columns: columns) {
-                    FirstNameAvtarView(firstName: "Sean")
-                    FirstNameAvtarView(firstName: "Sean")
-                    FirstNameAvtarView(firstName: "Sean")
-                    FirstNameAvtarView(firstName: "Sean")
-                    FirstNameAvtarView(firstName: "Sean")
-                    FirstNameAvtarView(firstName: "Sean")
-                    FirstNameAvtarView(firstName: "Sean")
-                }
-                
-                Spacer()
                 
             }
-            .navigationTitle("Location Name")
-            .navigationBarTitleDisplayMode(.inline)
+            
+            Text("Who's Here?")
+                .bold()
+                .font(.title2)
+            
+            LazyVGrid(columns: columns) {
+                FirstNameAvtarView(firstName: "Sean")
+                FirstNameAvtarView(firstName: "Sean")
+                FirstNameAvtarView(firstName: "Sean")
+                FirstNameAvtarView(firstName: "Sean")
+                FirstNameAvtarView(firstName: "Sean")
+                FirstNameAvtarView(firstName: "Sean")
+                FirstNameAvtarView(firstName: "Sean")
+            }
+            
+            Spacer()
             
         }
+        .navigationTitle("Location Name")
+        .navigationBarTitleDisplayMode(.inline)
+        
     }
 }
 
+
 struct LocationDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationDetailView()
+        NavigationView {
+            LocationDetailView()
+        }
     }
 }
 
