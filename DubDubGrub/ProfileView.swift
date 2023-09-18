863277b11bff1671adaf22ab217c542a9a9b71f8
Joel Storr <joel.storr@ewe.net>
2023-09-18 12:45:32 +0200

    Started Profile UI Layout.

--- DubDubGrub/ProfileView.swift
+++ DubDubGrub/ProfileView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    @State var charNumber: Int = 100
+    
     var body: some View {
-        Text("Profile View")
+        NavigationView {
+            VStack{
+                HStack{
+                    Image("default-avatar")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 80)
+                        .clipShape(Circle())
+                    VStack{
+                        Text("First Name")
+                            .font(.headline)
+                            .bold()
+                        Text("Last Name")
+                            .font(.headline)
+                            .bold()
+                        Text("Job Description")
+                            .font(.caption)
+                        
+                    }
+                    Spacer()
+                }
+                .background(Color(.secondarySystemBackground))
+                
+                HStack{
+                    Text("Bio: \(charNumber) characters remain ")
+                    Spacer()
+                    Button() {
+                        return
+                    } label: {
+                        Label("Check Out", image: "place")
+                            
+                    }
+
+                }
+                .padding()
+                Spacer()
+            }
+            .navigationTitle("Profile")
+            
+        }
     }
 }
 
