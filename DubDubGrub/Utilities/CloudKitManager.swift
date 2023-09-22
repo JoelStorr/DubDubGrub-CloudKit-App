4e14549460a904b49e9a0744a7c43e5550f3913f
Joel Storr <joel.storr@ewe.net>
2023-09-22 10:48:32 +0200

    Added Basic Query to CloudKitManger

--- DubDubGrub/Utilities/CloudKitManager.swift
+++ DubDubGrub/Utilities/CloudKitManager.swift
@@ -13,13 +13,40 @@ struct CloudKitManager{
     
     static func getLocations(completed: @escaping (Result<[DDGLocation], Error>) -> Void) {
         //Sorts the Elements by Name in a decending order
-        let sortDescriptor = NSSortDescriptor(key: DDGLocation.kName, ascending: true)
+        let alphabeticalSort = NSSortDescriptor(key: DDGLocation.kName, ascending: true)
         
         
         
         //Uses Convinience API
         //Query: return all DDGLocations
         let query = CKQuery(recordType: RecordType.location, predicate: NSPredicate(value: true))
+        query.sortDescriptors = [alphabeticalSort]
+        
+        //Using the Query
+        CKContainer.default().publicCloudDatabase.perform(query, inZoneWith: nil) { records, error in
+            //Check if there are any errors
+            guard error == nil else {
+                completed(.failure(error!))
+                return
+            }
+            
+            //Check if we have data
+            guard records == records else {
+                return
+            }
+            
+            
+            var locations: [DDGLocation] = []
+            
+            //Transforms record types into DDGLoaction Types
+            for record in records! {
+                let location = DDGLocation(record: record)
+                locations.append(location)
+            }
+            
+            completed(.success(locations))
+            
+        }
     }
     
     
