ed7f3638d4a8fa825d855a7039ed199ac7a8c37d
Joel Storr <joel.storr@ewe.net>
2023-09-22 12:09:56 +0200

    Reworkd depricated network call

--- DubDubGrub/Utilities/CloudKitManager.swift
+++ DubDubGrub/Utilities/CloudKitManager.swift
@@ -22,31 +22,65 @@ struct CloudKitManager{
         let query = CKQuery(recordType: RecordType.location, predicate: NSPredicate(value: true))
         query.sortDescriptors = [alphabeticalSort]
         
-        //Using the Query
-        CKContainer.default().publicCloudDatabase.perform(query, inZoneWith: nil) { records, error in
-            //Check if there are any errors
-            guard error == nil else {
-                completed(.failure(error!))
-                return
+        
+    CKContainer.default().publicCloudDatabase.fetch(withQuery: query, inZoneWith: nil) { result in
+                
+                switch result {
+                    case .failure(let error):
+                        completed(.failure(error))
+                        return
+                case .success((let matchResults, _)):
+                    
+                        var locations: [DDGLocation] = []
+                        
+                        for matchResult in matchResults {
+                            switch matchResult.1 {
+                                case .success(let record):
+                                    let location = DDGLocation(record: record)
+                                    locations.append(location)
+                                case .failure(let error):
+                                print("Error: \(error)")
+                                    return
+                            }
+                        }
+                        completed(.success(locations))
+                }
             }
-            
-            //Check if we have data
-            guard records == records else {
-                return
-            }
-            
-            
-            var locations: [DDGLocation] = []
-            
-            //Transforms record types into DDGLoaction Types
-            for record in records! {
-                let location = DDGLocation(record: record)
-                locations.append(location)
-            }
-            
-            completed(.success(locations))
-            
-        }
+        
+        
+        
+        
+        
+        
+        /* Depricated:
+         //Using the Query
+         CKContainer.default().publicCloudDatabase.perform(query, inZoneWith: nil) { records, error in
+         //Check if there are any errors
+         guard error == nil else {
+         completed(.failure(error!))
+         return
+         }
+         
+         //Check if we have data
+         guard records == records else {
+         return
+         }
+         
+         
+         var locations: [DDGLocation] = []
+         
+         //Transforms record types into DDGLoaction Types
+         for record in records! {
+         let location = DDGLocation(record: record)
+         locations.append(location)
+         }
+         
+         completed(.success(locations))
+         
+         }
+         
+         
+         */
     }
     
     
