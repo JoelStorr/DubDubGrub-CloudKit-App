f768f510cf5f31be3ebd4d52d4aaa67cd96abe27
Joel Storr <joel.storr@ewe.net>
2023-09-27 11:54:20 +0200

    Added UIImage extesnion to convert images to CKAssets

--- DubDubGrub/Utilities/Extensions/UIImage+Ext.swift
+++ DubDubGrub/Utilities/Extensions/UIImage+Ext.swift
@@ -5,4 +5,37 @@
 //  Created by Joel Storr on 27.09.23.
 //
 
-import Foundation
+
+import CloudKit
+import UIKit
+
+extension UIImage {
+    
+    func convertToCKAsset() -> CKAsset?{
+        
+        //Get apps bsed document directory url
+        guard let urlPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("Document directory came back nil")
+            return nil
+        }
+        
+        //Append some unique identifier for our profile image
+        let fileUrl = urlPath.appendingPathComponent("selectedAvatarImage")
+        
+        //Write the image data to the location the adress points to
+        guard let imageData = jpegData(compressionQuality: 0.25) else {
+            print("Could not compress image")
+            return nil
+        }
+        
+        //Create CKAsset with file url
+        do{
+            try imageData.write(to: fileUrl)
+            return CKAsset(fileURL: fileUrl)
+        }catch{
+            print("Could not write the image")
+            return nil
+        }
+    }
+}
+
