0708fe0db5830f95e1b0e78560a922843e25856e
Joel Storr <joel.storr@ewe.net>
2023-10-11 22:55:30 +0200

    Added voiceover labels to profile view

--- DubDubGrub/Views/Screens/ProfileView/ProfileView.swift
+++ DubDubGrub/Views/Screens/ProfileView/ProfileView.swift
@@ -26,6 +26,10 @@ struct ProfileView: View {
                             AvatarView(image: viewModel.avatar,size: 84)
                             EditImage()
                         }
+                        .accessibilityElement(children: .ignore)
+                        .accessibilityAddTraits(.isButton)
+                        .accessibilityLabel(Text("Profile Photo"))
+                        .accessibilityHint(Text("Opens the iPhone's photo picker"))
                         .padding(.leading, 12)
                         .onTapGesture { viewModel.isShowingPhotoPicker = true }
                         VStack(spacing: 1){
@@ -41,6 +45,7 @@ struct ProfileView: View {
                 VStack(alignment: .leading, spacing: 8){
                     HStack{
                         CharactersRemainView(currentCount: viewModel.bio.count)
+                            .accessibilityAddTraits(.isHeader)
                         Spacer()
                         if viewModel.isCheckedIn {
                             Button{
@@ -55,11 +60,13 @@ struct ProfileView: View {
                                     .background(Color.grubRed)
                                     .cornerRadius(3)
                             }
+                            .accessibilityLabel(Text("Check out of current location."))
                         }
                     }
                     TextEditor(text: $viewModel.bio)
                         .frame(height: 100)
                         .overlay(RoundedRectangle(cornerRadius: 8).stroke( Color.secondary, lineWidth: 1))
+                        .accessibilityHint(Text("This text field is for your bio and has a 100 character maximum."))
                 }
                 .padding(.horizontal, 20)
                 
