eb8c89909ae42611847cd83b5b8237a9f8f3dfa3
Joel Storr <joel.storr@ewe.net>
2023-10-14 20:18:15 +0200

    Reworkd depricated overlays

--- DubDubGrub/Views/Screens/ProfileView/ProfileView.swift
+++ DubDubGrub/Views/Screens/ProfileView/ProfileView.swift
@@ -171,7 +171,7 @@ struct BioTextEditor: View {
     var body: some View {
         TextEditor(text: text)
             .frame(height: 100)
-            .overlay(RoundedRectangle(cornerRadius: 8).stroke( Color.secondary, lineWidth: 1))
+            .overlay{ RoundedRectangle(cornerRadius: 8).stroke( Color.secondary, lineWidth: 1) }
             .accessibilityHint(Text("This text field is for your bio and has a 100 character maximum."))
     }
 }
