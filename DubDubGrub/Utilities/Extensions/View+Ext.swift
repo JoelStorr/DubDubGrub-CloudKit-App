66fb8b0aaba06f3b102cd569ab2890c3c9925053
Joel Storr <joel.storr@ewe.net>
2023-10-15 17:31:22 +0200

    Allowed user to cycle through text fields

--- DubDubGrub/Utilities/Extensions/View+Ext.swift
+++ DubDubGrub/Utilities/Extensions/View+Ext.swift
@@ -31,12 +31,6 @@ extension View {
             }
         }
     }
-    
-    
-    
-    func dismissKeyboard(){
-        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-    }
 }
 
 
